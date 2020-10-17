848c2cdf777f67a6bad3bfb481fcc4eb6c161cd0
Louai <baydoun.louai@gmail.com>
2020-10-17 11:58:55 +0300

    Updates

---
+++ precache-manifest.fda18ce975805a16dee3892aa4816532.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9028d442b6ced9505785d04bbad95ae6",
+    "url": "/genshin/index.html"
+  },
+  {
+    "revision": "f13c72a6ab7708144c69",
+    "url": "/genshin/static/css/main.fdb57a04.chunk.css"
+  },
+  {
+    "revision": "90ec28450ccb474fd934",
+    "url": "/genshin/static/js/2.79b271e7.chunk.js"
+  },
+  {
+    "revision": "4df4c58fd3db8af31000da1d75d059ea",
+    "url": "/genshin/static/js/2.79b271e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f13c72a6ab7708144c69",
+    "url": "/genshin/static/js/main.a9fde67f.chunk.js"
+  },
+  {
+    "revision": "b9a646262f66f07c7e44",
+    "url": "/genshin/static/js/runtime-main.fa725f99.js"
+  }
+]);
